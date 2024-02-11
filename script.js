d7ca44ed5b0a3a68afe4bd38302fe9db6b29471d
prakriti-2908 <77171197+prakriti-2908@users.noreply.github.com>
2024-02-12 00:08:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
-const arr = [1, 2, 3, 4, 1, 0, 2, 2];
+// const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
-	for(let i=0;i<arr.length;i++){
-		let subarr = [arr[i]];
-		let sum = subarr;
-		for(let j=0;j<arr.length;j++){
-			if(j==i){
-				continue;
-			}else{
-				
-				if(sum<=n){
-					subarr.push(arr[j]);
-				}
-				sum+=arr[j];
-			}
-		}
-	}
+	let ans = [];
+    for(let i=0;i<arr.length;i++){
+        sub = [arr[i]];
+        let sum = arr[i];
+        for(let j=0;j<arr.length;j++){
+            if(i==j){
+                continue;
+            }
+            sum += arr[j];
+            if(sum<=n){
+                sub.push(arr[j]);
+            }else {
+                ans.push(sub);
+                sub = [];
+                break;
+            }
+        }
+    }
+    if(sub.length != 0){
+        ans.push(sub);
+    }
+    return ans;
 };
 
 const n = prompt("Enter n: ");
